17d9d2c11cd5d20495e94ab74ee5ef4c903be7c4
石井日向 <hyu825@gmail.com>
2024-01-07 22:10:28 +0900

    レイアウト修正

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -5,25 +5,22 @@ class RanksController < ApplicationController
     @ranks = case period
             when 'month'
               ranks_month = User
-              .select('users.id, users.name, users.avatar, round(sum(records.quantity * drinks.volume * drinks.degree/100 * 0.8)) AS total_quantity')
+              .select('users.id, users.name, round(sum(records.quantity * drinks.volume * drinks.degree/100 * 0.8)) AS total_quantity','RANK() OVER (ORDER BY round(sum(records.quantity * drinks.volume * drinks.degree/100 * 0.8)) DESC) AS user_rank')
               .joins(records: :drink)
               .group('users.id')
               .where(records: { date: Date.today.beginning_of_month..Date.today.end_of_month })
-              .order(total_quantity: :desc)
             when 'week'
               ranks_week = User
-              .select('users.id, users.name, users.avatar, round(sum(records.quantity * drinks.volume * drinks.degree/100 * 0.8)) AS total_quantity')
+              .select('users.id, users.name, round(sum(records.quantity * drinks.volume * drinks.degree/100 * 0.8)) AS total_quantity','RANK() OVER (ORDER BY round(sum(records.quantity * drinks.volume * drinks.degree/100 * 0.8)) DESC) AS user_rank')
               .joins(records: :drink)
               .group('users.id')
               .where(records: { date: Date.today.beginning_of_week..Date.today.end_of_week })
-              .order(total_quantity: :desc)
             when 'day'
               ranks_day = User
-              .select('users.id, users.name, users.avatar, round(sum(records.quantity * drinks.volume * drinks.degree/100 * 0.8)) AS total_quantity')
+              .select('users.id, users.name, round(sum(records.quantity * drinks.volume * drinks.degree/100 * 0.8)) AS total_quantity','RANK() OVER (ORDER BY round(sum(records.quantity * drinks.volume * drinks.degree/100 * 0.8)) DESC) AS user_rank')
               .joins(records: :drink)
               .group('users.id')
               .where(records: { date: Date.today.beginning_of_day..Date.today.end_of_day })
-              .order(total_quantity: :desc)
             end
 
     @ranks_time = case period

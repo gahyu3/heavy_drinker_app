236133851661c19c748f2052de200ad89fd162f6
石井日向 <hyu825@gmail.com>
2024-01-09 00:34:30 +0900

    利用規約・プライバシーポリシー設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login, only: %i[top privacy]
   
   def top; end
+
+  def privacy; end
+
+  def rule; end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root 'static_pages#top'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'rule', to: 'static_pages#rule'
   
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'

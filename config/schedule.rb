cce1b13239c3f9781c1eb01083889cf3e4c6165f
石井日向 <hyu825@gmail.com>
2024-01-16 15:46:18 +0900

    whenevrの本番環境の適用

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,11 +3,9 @@
 # Rails.rootを使用するために必要
 require File.expand_path("#{File.dirname(__FILE__)}/environment")
 
-# cronを実行する環境変数
-rails_env = ENV['RAILS_ENV'] || :development
 
 # cronを実行する環境変数をセット
-set :environment, rails_env
+set :environment, :production
 
 # cronのログの吐き出し場所
 set :output, "#{Rails.root}/log/cron.log"

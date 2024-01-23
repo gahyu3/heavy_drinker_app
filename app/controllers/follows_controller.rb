b9e791a095b58f1da74cc7666da8ece5c449e36b
石井日向 <hyu825@gmail.com>
2024-01-23 19:46:01 +0900

    ユーザー検索ページレイアウト整え

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,13 +3,13 @@ class FollowsController < ApplicationController
   def create
       user = User.find(params[:user_id])
       current_user.active_follow.create(followed: user)
-      redirect_to records_path, success: 'hehe'
+      redirect_to users_path
   end
 
   def destroy
     user = User.find(params[:user_id])
     current_user.active_follow.find_by(followed_id: user).destroy
-    redirect_to records_path, warning: 'はずしました'
+    redirect_to users_path
   end
 
   def followings
